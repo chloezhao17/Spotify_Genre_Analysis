d5de57a859c6992aeb2fb3409e0e63f296873e64
Patrick Patin <40468092+patrickhpatin@users.noreply.github.com>
2020-05-17 12:14:03 -0400

    Basic DB Connection (working)

---
+++ config.py
@@ -0,0 +1,10 @@
+# Spotify API Key
+API_KEY = "<YOUR KEY HERE>"
+
+# AWS Database Information
+POSTGRES_USERNAME = "YOUR USER NAME"
+POSTGRES_PASSWORD = "YOUR PASSWORD"
+POSTGRES_ADDRESS = "mypostgresdb.cjcmsklygytk.us-east-2.rds.amazonaws.com"
+POSTGRES_PORT = "5432"
+POSTGRES_DBNAME = "my_data_class_db"
+Instructional_webSite = "https://blog.panoply.io/connecting-jupyter-notebook-with-postgresql-for-python-data-analysis"
